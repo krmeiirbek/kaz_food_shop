57a2479540807cd977fa69fd1aae1f88f93d954c
Kazybek <newkazybek@gmail.com>
2022-03-10 12:49:36 +0600

    bottom nav bar is done

--- lib/pages/food/popular_food_detail.dart
+++ lib/pages/food/popular_food_detail.dart
@@ -86,6 +86,7 @@ class PopularFoodDetail extends StatelessWidget {
           ),
         ),
         child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Container(
               padding: EdgeInsets.only(
@@ -106,6 +107,22 @@ class PopularFoodDetail extends StatelessWidget {
                   SizedBox(width: Dimensions.width5),
                   const Icon(Icons.add, color: AppColors.signColor),
                 ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(
+                top: Dimensions.height20,
+                bottom: Dimensions.height20,
+                left: Dimensions.width20,
+                right: Dimensions.width20,
+              ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(Dimensions.radius20),
+                color: AppColors.mainColor,
+              ),
+              child: const BigText(
+                text: "\$10 | Add to cart",
+                color: Colors.white,
               ),
             ),
           ],
